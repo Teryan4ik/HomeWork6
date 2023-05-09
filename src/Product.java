c910c168d8e72d89ffb59911937c02e007c9def4
Jollly_Rodger <andrey.teryannikov@gmail.com>
2023-05-09 17:41:06 +0300

    Submitting homework6

--- src/Product.java
+++ src/Product.java
@@ -1,96 +1,37 @@
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 public class Product {
-
-    private final String type;
-    private int price;
+    private static int countAllProducts = 0;
+    final private int id;
+    private ProductType productType;
+    private BigDecimal price;
     private boolean discount;
-    private LocalDate createDate;
+    private LocalDate addDate;
 
-
-    public Product(String type, int price, boolean discount, LocalDate createDate) {
-        this.type = type;
+    public Product(ProductType productType, BigDecimal price, boolean discount, LocalDate addDate) {
+        this.id = countAllProducts;
+        this.productType = productType;
         this.price = price;
         this.discount = discount;
-        this.createDate = createDate;
+        this.addDate = addDate;
+        countAllProducts++;
     }
 
-    public Product(String type, int price, boolean discount) {
-        this.type = type;
-        this.price = price;
-        this.discount = discount;
+    public ProductType getProductType() {
+        return productType;
     }
 
-    public Product(String type, int price) {
-        this.type = type;
-        this.price = price;
+    public void setProductType(ProductType productType) {
+        this.productType = productType;
     }
 
-    public static void findProductBuyPrice(List<Product> list, String name, int cost) {
-        List<Product> productList = list.stream()
-                .filter(product -> product.getType().equals(name) & product.getPrice() > cost)
-                .collect(Collectors.toList());
-        System.out.println(productList);
-    }
-
-    public static void findProductBuyDiscount(List<Product> list, String name, Boolean discount) {
-        List<Product> productList = list.stream()
-                .filter(product -> product.getType().equals(name) & product.isDiscount() == discount).
-                peek(p -> p.setPrice((int) (p.getPrice() * 0.9))).collect(Collectors.toList());
-        System.out.println(productList);
-    }
-
-    public static void findCheapProduct(List<Product> list, String name) {
-        List<Product> productList = Collections.singletonList(list.stream()
-                .filter(product -> product.getType().equals(name))
-                .min(Comparator.comparing(Product::getPrice))
-                .orElseThrow(() -> new RuntimeException("Продукт " + name + " не знайдено")));
-        System.out.println(productList);
-    }
-
-    public static void findLastAddProduct(List<Product> list, int count) {
-        List<Product> productList = list.stream()
-                .skip(Math.max(0, list.size() - count))
-                .toList();
-        System.out.println(productList);
-    }
-
-    public static void findProductBuyYearAndPrice(List<Product> list, int year, String name, int price) {
-        int collect2 = list.stream()
-                .filter(product -> product.getCreateDate() ==
-                        product.getCreateDate().withYear(year)
-                        & product.getType().equals(name)
-                        & product.getPrice() < price).mapToInt(Product::getPrice).sum();
-        System.out.println("Добавлено в " + year + " продукт " + name + " на сумму: " + collect2);
-    }
-
-    public static void groupByProduct(List<Product> list) {
-        Map<String, List<Product>> mapOfLists = list.stream()
-                .collect(Collectors.groupingBy(Product::getType));
-        System.out.println(mapOfLists);
-    }
-    @Override
-    public String toString() {
-        return "Product{" +
-                "type='" + type + '\'' +
-                ", price=" + price +
-                ", date=" + createDate +
-                '}';
-    }
-    public String getType() {
-        return type;
-    }
-
-    public int getPrice() {
+    public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
+    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
@@ -98,7 +39,39 @@ public class Product {
         return discount;
     }
 
-    public LocalDate getCreateDate() {
-        return createDate;
+    public void setDiscount(boolean discount) {
+        this.discount = discount;
+    }
+
+    public LocalDate getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(LocalDate addDate) {
+        this.addDate = addDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productType=" + productType +
+                ", price=" + price +
+                ", discount=" + discount +
+                ", addDate=" + addDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && discount == product.discount && productType == product.productType && Objects.equals(price, product.price) && Objects.equals(addDate, product.addDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, productType, price, discount, addDate);
     }
 }
